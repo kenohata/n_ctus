b6a7cfbca0cdbe084d017561f9f5b190d71a5ebe
Ken Ohata <ohata@planforgrowth.org>
2012-01-04 00:31:35 +0900

    change development.rb for sending confirm mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,16 @@ Connectus::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  #setting for devise confirm
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => '<username>',
+    :password             => '<password>',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true 
+    }
 end
