f0e79cb3ab9aec09c7369360f4fcd2b4594e70d9
Ken Ohata <ohata@planforgrowth.org>
2012-01-03 22:34:07 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Connectus::Application.config.secret_token = '20ecac73ec46ced50e799bdc0b107f1c244648870a4a8e9eee9ef608db16e0708559c4ff35856d70e2f5ac92fd724f42a7ccd67408cb5a33ebfdd6c59c5357b8'
